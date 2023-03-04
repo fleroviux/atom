612d3a7824dbdc04cb62f04132f4f06b2e4689d5
fleroviux <fleroviux@gmail.com>
2023-03-04 18:27:16 +0100

    Atom: Common: add f32 and f64 shorthand types

---
+++ atom/common/include/atom/float.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#ifdef ATOM_SCOPE_FLOAT_TYPES
+namespace atom {
+#endif
+
+  using f32 = float;
+  using f64 = double;
+
+#ifdef ATOM_SCOPE_FLOAT_TYPES
+} // namespace atom
+#endif
