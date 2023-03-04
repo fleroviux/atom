20f1b04b43d3d047d2ea5a4d667c14db8b250715
fleroviux <fleroviux@gmail.com>
2023-03-04 18:19:46 +0100

    Atom: Common: add i8, i16, i32 and i64 alternative types

--- atom/common/include/atom/integer.hpp
+++ atom/common/include/atom/integer.hpp
@@ -8,14 +8,21 @@
 namespace atom {
 #endif
 
-  using u8 = std::uint8_t;
-  using s8 = std::int8_t;
+  using u8  = std::uint8_t;
   using u16 = std::uint16_t;
-  using s16 = std::int16_t;
   using u32 = std::uint32_t;
-  using s32 = std::int32_t;
   using u64 = std::uint64_t;
+
+  using s8  = std::int8_t;
+  using s16 = std::int16_t;
+  using s32 = std::int32_t;
   using s64 = std::int64_t;
+
+  using i8  = s8;
+  using i16 = s16;
+  using i32 = s32;
+  using i64 = s64;
+
   using uint = unsigned int;
   using size_t = std::size_t;
 
